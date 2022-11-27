bf0bd15cab78e9598290310b7cba1c0de7d659fe
Jeev Pangli <jspanglez@gmail.com>
2022-11-27 16:10:31 +0000

    First question page

--- app/src/main/java/com/example/mobileappdevcoursework/MainActivity.kt
+++ app/src/main/java/com/example/mobileappdevcoursework/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mobileappdevcoursework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val next = findViewById<Button>(R.id.buttonNext)
+        next.isEnabled = false
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        val next = findViewById<Button>(R.id.buttonNext)
+        next.isEnabled = true
     }
 }
