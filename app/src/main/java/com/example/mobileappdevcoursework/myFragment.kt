5397d94864421f57eb3539e336edeee3d60b8fda
Jspanglez <jeevpangli@gmail.com>
2022-11-28 10:50:44 +0000

    Added fragments

---
+++ app/src/main/java/com/example/mobileappdevcoursework/myFragment.kt
@@ -0,0 +1,23 @@
+package com.example.mobileappdevcoursework
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class myFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater?.inflate(R.layout.question1_fragment,
+            container, false)
+
+        return view
+    }
+}
